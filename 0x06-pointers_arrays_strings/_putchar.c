b4cc8120f2e0d7c3f839474c028de37edf9e8fa8
VictorCodeX-tech <victorkimotho60@gmail.com>
2023-06-29 13:41:39 -0700

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - writes to stdout
+ * @c: The character to print
+ *
+ * Return: 1
+ */
+
+
+
+
+
+
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
